0b7f97cb1e775302f61e1986dc5f4c5eabb56e1e
ukyo <deadzq@qq.com>
2019-07-09 13:50:52 +0800

    simpler

--- students-data-service/src/main/java/com/ykmimi/studentsdataservice/StudentsDataServiceApplication.java
+++ students-data-service/src/main/java/com/ykmimi/studentsdataservice/StudentsDataServiceApplication.java
@@ -1,10 +1,12 @@
 package com.ykmimi.studentsdataservice;
 
+import brave.sampler.Sampler;
 import com.ykmimi.studentsdataservice.utils.PortUtil;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableEurekaClient
@@ -13,6 +15,10 @@ public class StudentsDataServiceApplication {
     public static void main(String[] args) {
         int port = PortUtil.setPort(3,"请输入StudentsDataServiceApplication的端口号,推荐8001,8002,8003.",8001);
         new SpringApplicationBuilder(StudentsDataServiceApplication.class).properties("server.port="+port).run(args);
+    }
+    @Bean
+    public Sampler defaultSampler(){
+        return Sampler.ALWAYS_SAMPLE;
     }
 
 }
--- students-view-service-feign/src/main/java/com/ykmimi/studentsviewservicefeign/StudentsViewServiceFeignApplication.java
+++ students-view-service-feign/src/main/java/com/ykmimi/studentsviewservicefeign/StudentsViewServiceFeignApplication.java
@@ -1,11 +1,13 @@
 package com.ykmimi.studentsviewservicefeign;
 
+import brave.sampler.Sampler;
 import com.ykmimi.eurekaserver.utils.PortUtil;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableEurekaClient
@@ -16,6 +18,10 @@ public class StudentsViewServiceFeignApplication {
     public static void main(String[] args) {
         int port = PortUtil.setPort(0,"请输入StudentsViewServiceFeignApplication的端口号,推荐8012.",8012);
         new SpringApplicationBuilder(StudentsViewServiceFeignApplication.class).properties("server.port="+port).run(args);
+    }
+    @Bean
+    public Sampler defaultSampler(){
+        return Sampler.ALWAYS_SAMPLE;
     }
 
 }
